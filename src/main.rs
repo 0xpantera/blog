d15a698bc831d200527079f1e71729be24437a04
Francososa <franky.sosa@gmail.com>
2022-04-19 18:42:05 -0600

    Implement requesting a review of the post to change its state

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
-
+use blog::Post;
 
 fn main() {
-    println!("test");
+    let mut post = Post::new();
+
+    post.add_text("test text");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("test text", post.content());
 }
